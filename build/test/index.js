a1bf3d6674023ce543df76177f13d6f07ffa328f
leandro.jm <leandro.jm@gmail.com>
2025-04-01 18:03:24 -0300

    first commit

---
+++ build/test/index.js
@@ -0,0 +1,51 @@
+const NWS_API_BASE = "http://archi-track.lmlabs.com.br";
+const USER_AGENT = "archi-track-app/1.0";
+const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInJvbGVOYW1lIjoibWVtYmVyIiwiaWF0IjoxNzQyNTE5NDM1LCJleHAiOjE3NDUxMTE0MzV9.8EI70DvVAV3EVjslx2UQENGEtfDbs6_ezbCI6cCNpqc";
+async function makeRequest(url) {
+    const headers = {
+        "User-Agent": USER_AGENT,
+        Accept: "application/json",
+        Authorization: TOKEN,
+    };
+    try {
+        const response = await fetch(url, { headers });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    }
+    catch (error) {
+        console.error("Error making request:", error);
+        return null;
+    }
+}
+async function test(application) {
+    async (application) => {
+        const applicationUrl = `${NWS_API_BASE}/api/application:get?filter=%7B%22name%22%3A%20%22${application}%22%7D`;
+        console.log(applicationUrl);
+        const applicationData = await makeRequest(applicationUrl);
+        console.log(applicationData);
+        if (!applicationData) {
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: "Failed to retrieve application data",
+                    },
+                ],
+            };
+        }
+        const applicationText = `A aplicação ${application} é ${applicationData?.data?.description_full}.`;
+        console.log(applicationText);
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: applicationText,
+                },
+            ],
+        };
+    };
+}
+test("Gcob");
+export {};
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const NWS_API_BASE = "http://archi-track.lmlabs.com.br";
+const USER_AGENT = "archi-track-app/1.0";
+const TOKEN =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInJvbGVOYW1lIjoibWVtYmVyIiwiaWF0IjoxNzQyNTE5NDM1LCJleHAiOjE3NDUxMTE0MzV9.8EI70DvVAV3EVjslx2UQENGEtfDbs6_ezbCI6cCNpqc";
+
+const server = new McpServer({
+  name: "archi-track",
+  version: "1.0.0",
+});
+
+type ApplicationResponse = {
+  data?: {
+    description_full?: string;
+  };
+};
+
+async function makeRequest<T>(url: string): Promise<T | null> {
+  const headers = {
+    "User-Agent": USER_AGENT,
+    Accept: "application/json",
+    Authorization: TOKEN,
+  };
+
+  try {
+    const response = await fetch(url, { headers });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error making request:", error);
+    return null;
+  }
+}
+
+server.tool(
+  "get-info-application",
+  "Get information about application.",
+  {
+    application: z.string().trim(),
+  },
+  async ({ application }) => {
+    const applicationUrl = `${NWS_API_BASE}/api/application:get?filter=%7B%22name%22%3A%20%22${application}%22%7D`;
+    const applicationData = await makeRequest<ApplicationResponse>(
+      applicationUrl
+    );
+
+    if (!applicationData) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Failed to retrieve application data",
+          },
+        ],
+      };
+    }
+
+    const applicationText = `A aplicação ${application} é ${applicationData.data?.description_full}.`;
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: applicationText,
+        },
+      ],
+    };
+  }
+);
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Archi track MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
